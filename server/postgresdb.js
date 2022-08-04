0423964dd3119ab4064c1ef170cacb78c6f07f52
Kevin Ha <kevincw.ha@gmail.com>
2022-08-04 16:05:33 -0700

    feat: update server

--- server/postgresdb.js
+++ server/postgresdb.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const postgres = require('postgres');
 const { Pool, Client } = require('pg');
 const Promise = require('bluebird');
@@ -6,7 +7,7 @@ const pool = new Pool ({
   host: process.env.POSTGRES_HOST,
   port: process.env.POSTGRES_PORT,
   database: process.env.DB_NAME,
-  username: process.env.POSTGRES_USER,
+  user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASS
 });
 
