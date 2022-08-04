59a26cc7f33cee31f74d23f21b3949fb5c99de36
Kevin Ha <kevincw.ha@gmail.com>
2022-08-03 17:54:40 -0700

    updated k6 stress test

--- test/review.get.test.js
+++ test/review.get.test.js
@@ -1,21 +1,37 @@
 import http from "k6/http";
 import { sleep } from 'k6';
 
+// export let options = {
+//   stages: [
+//     { duration: '15s', target: 10 },
+//     { duration: '30s', target: 10},
+//     { duration: '15s', target: 100 },
+//     { duration: '30s', target: 100 },
+//     { duration: '1m', target: 100 },
+//     { duration: '15s', target: 200 },
+//     { duration: '30s', target: 200 },
+//     { duration: '1m', target: 200 },
+//     { duration: '15s', target: 400 },
+//     { duration: '30s', target: 400 },
+//     { duration: '1m', target: 400 },
+//     { duration: '1m', target: 0 },
+//   ],
+// };
+
 export let options = {
-  stages: [
-    { duration: '15s', target: 10 },
-    { duration: '30s', target: 10},
-    { duration: '15s', target: 100 },
-    { duration: '30s', target: 100 },
-    { duration: '1m', target: 100 },
-    { duration: '15s', target: 200 },
-    { duration: '30s', target: 200 },
-    { duration: '1m', target: 200 },
-    { duration: '15s', target: 400 },
-    { duration: '30s', target: 400 },
-    { duration: '1m', target: 400 },
-    { duration: '1m', target: 0 },
-  ],
+  insecureSkipTLSVerify: true,
+  noConnectionReuse: false,
+  scenarios: {
+    constant_request_rate: {
+      executor: 'constant-arrival-rate',
+      rate: '300',
+      timeUnit: '1s',
+      //note: 1000 iterations per second
+      duration: '60s',
+      preAllocatedVUs: 20,
+      maxVUs: 200,
+    },
+  },
 };
 
 export default () => {
